19e1baaf3b37a7f7c5cfc253686fca0293046964
Joewan <jabwanjoe@gmail.com>
2025-09-21 23:58:56 -0400

    updating config

--- backend/finance/src/main/java/com/financeapp/finance/config/SecurityConfig.java
+++ backend/finance/src/main/java/com/financeapp/finance/config/SecurityConfig.java
@@ -2,15 +2,36 @@ package com.financeapp.finance.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     protected PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails user = User.builder()
+            .username("user")
+            .password(passwordEncoder.encode("password"))
+            .roles("USER")
+            .build();
+        UserDetails admin = User.builder()
+            .username("admin")
+            .password(passwordEncoder.encode("adminpassword"))
+            .roles("ADMIN", "USER")
+            .build();
+        return new InMemoryUserDetailsManager (admin, user);
     }
 
 }
