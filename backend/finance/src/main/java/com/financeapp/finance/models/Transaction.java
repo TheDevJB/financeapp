86433575d3ccd79793ac9d88402959d67c8a45aa
Joewan <jabwanjoe@gmail.com>
2025-10-04 16:29:53 -0400

    added springsecurity and fixed model entity

--- backend/finance/src/main/java/com/financeapp/finance/models/Transaction.java
+++ backend/finance/src/main/java/com/financeapp/finance/models/Transaction.java
@@ -1,13 +1,13 @@
 package com.financeapp.finance.models;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -19,22 +19,16 @@ public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
-    private String transactionId;
+    @Column(name = "transaction_id")
+    private Long transactionId;
 
-    @Id
     @ManyToOne
-    @Column
-    private Long userId;
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(name = "dollar_amount")
     private BigDecimal dollarAmount;
 
     @Column(name = "description")
     private String description;
-
-    private LocalDateTime timestamp;
-    private String sourceAccountId;
-    private String transactionType;
-
 }
