bdcd15b488190db5236159b22a0829c706daf0c7
Joewan <jabwanjoe@gmail.com>
2025-11-19 01:32:51 -0500

    added test for UserService

--- backend/finance/src/main/java/com/financeapp/finance/repositories/UserRepository.java
+++ backend/finance/src/main/java/com/financeapp/finance/repositories/UserRepository.java
@@ -1,15 +1,20 @@
 package com.financeapp.finance.repositories;
+
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;   
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.financeapp.finance.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findById(Integer userId);
 
     Optional<User> findByUsername(String username);
+
     Optional<User> findByEmail(String email);
+
     Optional<User> findByUsernameOrEmailOrPhone(String email, String username, String phone);
 }
