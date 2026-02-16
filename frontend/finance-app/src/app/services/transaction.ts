62e2b09567cc97006978b55606a6fa35d6014133
Joewan <jabwanjoe@gmail.com>
2026-02-16 01:00:37 -0500

    adding transfer functionality to UI soon

---
+++ frontend/finance-app/src/app/services/transaction.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Transaction {
+  private apiUrl = 'http://localhost:8080/api/v1/transactions';
+
+  constructor(private http: HttpClient);
+}
